421659aff2b586b33d7d3293c02d9cec41aeccc0
Christopher Duggan <christopherduggan@gmail.com>
2021-08-04 03:26:12 -0400

    init

---
+++ part1.js
@@ -0,0 +1,55 @@
+/* In Part 1, we're going to create a Superhero list! Think back to previous
+ * lessons, help your classmates if things are going smoothly, and ask for help
+ * if you need it!
+ */
+
+
+// 1. create three superheroes with the same format as Nobody Man
+
+const nobodyMan = {
+  name: "Nobody Man",
+  secretIdentity: "Bob Fakeman",
+  powers: ['invisibility', 'can erase your memory of him'],
+  team: 'The Randos',
+  saying: "You don't know me!",
+}
+
+// Add your heroes here
+
+
+
+
+// 2. Create an array called "heroes" and put your hero objects in it
+
+
+
+
+/* 3. Create a loop that makes a <div> element for each hero in the array. The
+ * div should contain the following:
+ *    a. An <h2> element that contains the hero's name at the top
+ *    b. <p> elements that contain the hero's team and secret identity with
+ *    labels
+ *    c. An <ul> element that contains an <h3> element that contains "Powers:"
+ *    and an <li> element that contains each of hero's powers
+ *    d. A <p> element that has the hero's saying in quotes
+ *
+ *  The final product should look like this:
+ *
+ *  <div>
+ *    <h2>Nobody Man</h2>
+ *    <p>Team: The Randos</p>
+ *    <p>Secret Identity: Bob Fakeman</p>
+ *    <ul>
+ *      <h3>Powers:</h3>
+ *      <li>invisibility</li>
+ *      <li>can erase your memory of him</li>
+ *    </ul>
+ *    <p>"You don't know me!"</p>
+ *  </div>
+ *
+ * Finally, the loop should append the created div to the #hero-list element in
+ * the index.html.
+ */
+
+// Part 1 complete!
+
---
+++ part2.js
@@ -0,0 +1,41 @@
+/* In Part 2 we will create a form that will allow the user to create a new
+ * superhero to add to our list. You can do this!
+ */
+
+// 1. Create a form that has an input for the hero's name, secret identity,
+// powers, team, and their saying. Give the form a button that says "Add Hero" and append it to the #form-box div
+
+
+
+// 2. Add an event listener to the form's button that checks for a click, then
+// calls a function called "addHero."
+
+
+
+/* 3. Modify the addHero function so that it creates an object using the values
+ * in the form, then adds it to the hero array created in part1. Here is a reminder of
+ * what our hero objects from part 1 looked like:
+ *
+ * {
+ * name: "Nobody Man",
+ * secretIdentity: "Bob Fakeman",
+ * powers: ['invisibility', 'can erase your memory of him'],
+ * team: 'The Randos',
+ * saying: "You don't know me!",
+ * }
+ *
+ * After the addHero function adds our new hero to the array, rewrite the
+ * contents of the #hero-list div so that it includes the hero that's been just
+ * added
+ */
+
+function addHero() {
+
+}
+
+// Part 2 complete!
+
+/* Bonus Ideas:
+ * Add images for your heroes
+ * style up the page so it looks puuuuurty
+ */
